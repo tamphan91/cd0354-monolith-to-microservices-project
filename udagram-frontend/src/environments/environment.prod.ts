b5f9b13152020a5c8f1f0d00ecfa8b181991389a
tamphan91 <tamphan04101991@gmail.com>
2022-11-10 00:52:28 +0700

    finish

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa2abf315ca724bbdab464cbf7c0bfee-1507026924.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
